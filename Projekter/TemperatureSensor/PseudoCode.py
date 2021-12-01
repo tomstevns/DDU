59764e8d4ff264883e70331d0d9922d4451c83cd
tomstevns <tomstevns@gmail.com>
2021-12-01 12:48:22 +0100

    Pseudocode for Temperature Sensor

---
+++ Projekter/TemperatureSensor/PseudoCode.py
@@ -0,0 +1,41 @@
+#Pseudo functions
+def Initialize():
+    pass
+
+def ReadSensor():
+    pass
+
+def DataAcquisition():
+    pass
+
+def Alert():
+    pass
+
+def Display():
+    pass
+
+def Timer():
+    pass
+
+def Interrupt():
+    #Implement interrupt request
+    return False
+
+def Delay():
+    callable(Timer())
+    callable(Terminate())
+
+def Terminate():
+    if Interrupt() == False:
+        return False
+
+
+#Pseudo code
+callable(Initialize())
+def Main():
+    callable(ReadSensor())
+    callable(DataAcquisition())
+    callable(Alert())
+    callable(Display())
+    callable(Delay())
+
